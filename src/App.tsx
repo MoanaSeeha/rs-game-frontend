e2ec9423d7d0da7c7024cfdd74dd8e6dc054499d
Prometheus <96538997+MoanaSeeha@users.noreply.github.com>
2022-05-10 01:59:56 +0800

    fff

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@
 import React from "react";
 
 import "./App.css";
-import Header from "./pages/header";
 
 import Router from "./router";
 
 const App = () => {
   return (
     <div className="relative w-full">
-      <Header />
       <Router />
     </div>
   );
